58f5c349f753f2d44650c2477ff4edf688cd3d53
Sam Pisheh <sampisheh@Sams-iMac.local>
2018-09-10 13:47:27 -0700

    k_means done

--- k_means/k_means_cluster.py
+++ k_means/k_means_cluster.py
@@ -34,22 +34,29 @@ def Draw(pred, features, poi, mark_poi=False, name="image.png", f1_name="feature
     plt.xlabel(f1_name)
     plt.ylabel(f2_name)
     plt.savefig(name)
-    plt.show()
+    plt.savefig("kmeans.png")
+    #plt.show()
 
 
 
 ### load in the dict of dicts containing all the data on each person in the dataset
-data_dict = pickle.load( open("../final_project/final_project_dataset.pkl", "r") )
+data_dict = pickle.load( open("../final_project/final_project_dataset.pkl", "rb") )
 ### there's an outlier--remove it! 
 data_dict.pop("TOTAL", 0)
-
-
+"""maxi = 0
+mini = 100000000000
+for i in data_dict.items():
+    if i[1]["salary"]!="NaN":
+        maxi=max(maxi, i[1]["salary"])
+        mini=min(mini, i[1]["exercised_stock_options"])
+print(maxi, mini)"""
 ### the input features we want to use 
 ### can be any key in the person-level dictionary (salary, director_fees, etc.) 
 feature_1 = "salary"
 feature_2 = "exercised_stock_options"
+feature_3 = "total_payments"
 poi  = "poi"
-features_list = [poi, feature_1, feature_2]
+features_list = [poi, feature_1, feature_2, feature_3]
 data = featureFormat(data_dict, features_list )
 poi, finance_features = targetFeatureSplit( data )
 
@@ -58,19 +65,21 @@ poi, finance_features = targetFeatureSplit( data )
 ### you'll want to change this line to 
 ### for f1, f2, _ in finance_features:
 ### (as it's currently written, the line below assumes 2 features)
-for f1, f2 in finance_features:
+for f1, f2, f3 in finance_features:
     plt.scatter( f1, f2 )
-plt.show()
+plt.savefig("data.png")
 
 ### cluster here; create predictions of the cluster labels
 ### for the data and store them to a list called pred
 
-
-
+from sklearn.cluster import KMeans
+kmeans = KMeans(n_clusters=3).fit(data)
+#kmeans.fit(data)
+pred=kmeans.predict(data)
 
 ### rename the "name" parameter when you change the number of features
 ### so that the figure gets saved to a different file
 try:
     Draw(pred, finance_features, poi, mark_poi=False, name="clusters.pdf", f1_name=feature_1, f2_name=feature_2)
 except NameError:
-    print "no predictions object named pred found, no clusters to plot"
+    print ("no predictions object named pred found, no clusters to plot")
